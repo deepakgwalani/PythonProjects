31de12f799ca38555dda9fdf892f5fd06e3c67bf
Deepak Gwalani <deepak.gwalani@gmail.com>
2020-11-25 20:44:55 +0800

    apply fix

--- algorithms/peak_finding.py
+++ algorithms/peak_finding.py
@@ -1,19 +1,20 @@
-def peak_finding(arr, l, r):
+def peak_finding(arr, l, r, n):
     if l == r:
         # array of length 1
         return arr[l]
     mid = (l + r) // 2
-    if (mid == l and arr[mid] >= arr[mid + 1]) or (mid == r and arr[mid] >= arr[mid - 1]):
+    if (mid == 0 or arr[mid] >= arr[mid + 1]) and (mid == n-1 or arr[mid] >= arr[mid - 1]):
         return arr[mid]
     elif arr[mid] < arr[mid - 1]:
-        return peak_finding(arr, l, mid - 1)
+        return peak_finding(arr, l, mid - 1, n)
     else:
-        return peak_finding(arr, mid + 1, r)
+        return peak_finding(arr, mid + 1, r, n)
 
 
 def main():
-    arr = [5, 10]
-    print(peak_finding(arr, 0, len(arr) - 1))
+    arr = [10, 14, 15, 22, 23, 90, 67]
+    n = len(arr)
+    print(peak_finding(arr, 0, n-1, n))
 
 
 main()
